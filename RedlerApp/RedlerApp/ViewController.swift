da76a56c5901debe3d94e25edd3ee1b928e8a529
Mustafa Şevki Coşar <cosar.mustafasevki@gmail.com>
2017-12-21 17:52:53 +0300

    Create first page

--- RedlerApp/RedlerApp/ViewController.swift
+++ RedlerApp/RedlerApp/ViewController.swift
@@ -8,17 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    
+    let foods = ["Tech", "Business", "World", "Science", "Sport"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        pickerView.dataSource = self
+        pickerView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //pickerView funcitons
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return foods.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return foods[row]
+    }
+    
+    
+    // write to label that selected picker item.
+   /*func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        label.text = foods[row]
+    }*/
+    
+    
 
 
 }
